cb56ddff5c75879988b698468509a9db55c054e0
lupusalba <a145296@gmail.com>
2020-10-24 02:47:23 +0200

    first ruby code

---
+++ rubyPrimitiveCalc.rb
@@ -0,0 +1,17 @@
+# first self excersise
+# input two values, export *,/,+,-
+
+puts "input value A"
+a = gets.chomp.to_f
+puts "input val B"
+b = gets.chomp.to_i
+puts "result of + is:"
+multi = a * b
+divi = a / b
+subst = a - b
+plus = a + b
+
+puts "res of multi is #{multi}"
+puts "res of divi is #{divi}"
+puts "res of subst is #{subst}"
+puts "res of plus is #{plus}"
